2e285c6a7c20737a3a0a42ae0cfa934fa3ee72b4
Infernaton <lubin.pauly@gmail.com>
2021-11-17 15:10:22 +0100

    Add some template+route

---
+++ src/Controller/ConnectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConnectController extends AbstractController
+{
+    /**
+     * @Route("/login", name="connect")
+     */
+    public function index(): Response
+    {
+        return $this->render('connect/index.html.twig', [
+            'controller_name' => 'ConnectController',
+        ]);
+    }
+}
--- src/Controller/TricountController.php
+++ src/Controller/TricountController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Form\TricountType;
 use App\Entity\Tricount;
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route ("/tricount", name="tricount")
+ */
 class TricountController extends AbstractController
 {
     /**
-     * @Route("/", name="tricount")
+     * @Route("/", name="index")
      */
     public function index(Request $request): Response
     {
@@ -25,10 +30,20 @@ class TricountController extends AbstractController
             $entityManager->persist($tricount);
             $entityManager->flush();
 
-            return $this->redirection("add_participant", [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute("show", ["id"=>$tricount["id"]], Response::HTTP_SEE_OTHER);
         }
         return $this->render('tricount/index.html.twig', [
             'Tricount' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route ("/{id}", name="show", methods={"GET"})
+     */
+    public function tricountIndex(ParticipantRepository $participantRepository, Tricount $tricount): Response{
+        $triCount_participant = $participantRepository->findBy(["personindebt_id"=>$tricount]);
+        return $this->render('tricount/showParticipant.html.twig', [
+            "participants"=>$triCount_participant
         ]);
     }
 }
