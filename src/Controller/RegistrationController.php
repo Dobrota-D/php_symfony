1ac705ea815f216265ea8054f3f62fe7eae59c25
Infernaton <lubin.pauly@gmail.com>
2021-11-18 11:40:57 +0100

    Création d'utilisateur/Connexion avec un compte

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,11 +10,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_register")
+     * @Route("/sigin", name="sign")
      */
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
@@ -43,12 +44,20 @@ class RegistrationController extends AbstractController
         ]);
     }
     /**
-     * @Route ("/signin", name="sign")
+     * @Route ("/login", name="app_register")
      */
-    public function signIn(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('connect/index.html.twig', [
-            'controller_name' => 'S\'inscrire'
+        //return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('registration/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
+    }
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
